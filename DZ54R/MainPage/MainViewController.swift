9175d9697cb5f9ae00838bf07e1eac3bd3d79464
Memka <mkasenova87@gmail.com>
2023-06-03 18:17:37 +0600

    Four hw

---
+++ DZ54R/MainPage/MainViewController.swift
@@ -0,0 +1,8 @@
+//
+//  MainViewController.swift
+//  DZ54R
+//
+//  Created by merim kasenova on 27/5/23.
+//
+
+import Foundation
---
+++ DZ54R/UIComponents/TabBarController.swift
@@ -0,0 +1,8 @@
+//
+//  TabBarController.swift
+//  DZ54R
+//
+//  Created by merim kasenova on 27/5/23.
+//
+
+import Foundation
